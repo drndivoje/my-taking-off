e6859502bb429d5525cd55667dc75b28d534b92c
Ivan Doskovic:w <ivandoskovic@gmail.com>
2017-10-31 20:03:01 +0100

    Reorganaizing the modules

--- lib/rocketry.ex
+++ lib/rocketry.ex
@@ -1,42 +1,27 @@
-defmodule Converter do
-	def to_nearest_tenth(val) do 
-		Float.ceil val, 1
-	end
-
-	def to_km(val) do
-		val / 1000
-	end
-
-	def to_light_seconds({:miles, miles}) do
-		(miles * 5.36819e-6) |> round_down
-	end
-
-	def to_light_seconds({:meters, meters}) do
-		(meters * 3.335638620368e-9) |> round_down
-	end
-
-	def round_down(val) when is_float(val), do: trunc(val)
-end
-
-
+#The mass of Mars is 6.39e23kg and its radius is 3.4e6m
+#The mass of the Moon is 7.35e22kg and its radius is 1.738e6m
 defmodule Physics.Rocketry do
+	import Converter
+	import Calcs
+    import Physics.Laws
+    import Planets
 
-	def escape_velocity(:earth) do
-		%{mass: 5.97e24, radius: 6.371e6}
-		|> escape_velocity
-	end
+    def escape_velocity(:mars), do: mars |>escape_velocity
+    def escape_velocity(:moon), do: moon |> escape_velocity
+	def escape_velocity(:earth), do: earth |> escape_velocity
+
 
 	def escape_velocity(planet) when is_map(planet) do 
 		planet
 		|> calculate_escape 
-		|> Converter.to_km  
-		|> Converter.to_nearest_tenth
+		|> to_km  
+		|> to_nearest_tenth
 	end
 
    defp calculate_escape(%{mass: mass, radius: radius}) do 
-   	   newtons_contanst = 6.67e-11
-   	   2 * newtons_contanst * mass / radius
-   	   |> :math.sqrt
+   	   
+   	   2 * newtons_gravitational_constant * mass / radius
+   	   |> square_root
    end
 
 
